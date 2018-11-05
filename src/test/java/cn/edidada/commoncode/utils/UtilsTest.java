f92953e187f846981c899dc7d5c525aa6fc80600
edidada <1664884095@qq.com>
2018-11-06 00:25:45 +0800

    make project

---
+++ src/test/java/cn/edidada/commoncode/utils/UtilsTest.java
@@ -0,0 +1,16 @@
+package cn.edidada.commoncode.utils;
+
+import org.junit.Test;
+
+public class UtilsTest {
+
+
+    @Test
+    public void testAdd() {
+        String[] nameArr = StringUtils.tokenizeToStringArray("fdasfas,555555;6788 9999", ",; ");
+        for(String name : nameArr){
+            System.out.println(name);
+        }
+    }
+
+}
